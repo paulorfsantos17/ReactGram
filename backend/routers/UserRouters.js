2d21a8fa3b73df9e37284ea182c5d3558b74c6c3
Paulo Santos <paulorfsantos17@gmail.com>
2022-08-08 17:53:02 -0300

    end routes controler users

--- backend/routers/UserRouters.js
+++ backend/routers/UserRouters.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, getCurrentUser } = require("../controllers/UserController");
+const { register, login, getCurrentUser, update, getUserById } = require("../controllers/UserController");
 
 const validade = require("../middlewares/handleValidation")
-const {userCreateValidation, loginValidation} = require("../middlewares/userValidation")
+const {userCreateValidation, loginValidation, userUpdateValidation, } = require("../middlewares/userValidation")
 const authGuard = require("../middlewares/authGuard")
+const {imageUpload} = require("../middlewares/imageUpload")
 
 router.post("/register", userCreateValidation(), validade,register);
 router.post("/login", loginValidation(), validade,login);
 router.get("/profile", authGuard, getCurrentUser)
+router.put("/", authGuard, userUpdateValidation(), validade,imageUpload.single("profileImage"), update )
+router.get("/:id", getUserById)
 
 module.exports = router;
 
